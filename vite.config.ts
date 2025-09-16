41307a26dfa67d8638a608d8d28867b22fae0142
Davi de Carvalho <davi_carvalho96@hotmail.com>
2025-09-15 22:31:43 -0300

    configuração pra botar pra rodar no github

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  // base: "/primeiraversao-landingpage/",
+  base: "/primeiraversao-landingpage/",
   plugins: [
     react(),
     tailwindcss(),
